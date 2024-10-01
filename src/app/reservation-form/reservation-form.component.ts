075d282d5c780987ced1accd57621a5fb1255f06
kopiPejst92 <agkop01@gmail.com>
2024-10-01 14:52:36 +0200

    project initiation plus form validation

---
+++ src/app/reservation-form/reservation-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reservation-form',
+  templateUrl: './reservation-form.component.html',
+  styleUrls: ['./reservation-form.component.css']
+})
+export class ReservationFormComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
+  
+  constructor(private formBuilder: FormBuilder){
+    this.reservationForm=this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(): void {
+    if(this.reservationForm.valid){
+      console.log("valid")
+    }
+  }
+}
---
+++ src/app/reservation/reservation.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    ReservationFormComponent,
+    ReservationListComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class ReservationModule { }

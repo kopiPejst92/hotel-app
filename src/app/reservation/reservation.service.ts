578c561aafe757a4dd83dae813afc7f92609bd0e
kopiPejst92 <agkop01@gmail.com>
2024-10-02 16:23:00 +0200

    crud operations init

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+  
+  getReservations(): Reservation[] {
+    return this.reservations;
+  } 
+
+  getReservation(id: string): Reservation  | undefined{
+    return this.reservations.find(res => res.id == id);
+  } 
+
 }

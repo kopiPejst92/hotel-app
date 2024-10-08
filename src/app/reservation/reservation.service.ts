5d41ef81167a1b3d18b051b9dcc8e473ae58d2f3
kopiPejst92 <agkop01@gmail.com>
2024-10-08 11:58:08 +0200

    Getting rid of local storage

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,11 +8,6 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
-  constructor(){
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-  }
-
   getReservations(): Reservation[] {
     return this.reservations;
   }
@@ -22,23 +17,20 @@ export class ReservationService {
   }
 
   addReservation(reservation: Reservation): void {
-    let lastElementIndex = this.reservations.length ? this.reservations[this.reservations.length-1].id: 0;
-    reservation.id=lastElementIndex + 1;
+    let lastElementIndex = this.reservations.length ? this.reservations[this.reservations.length - 1].id : 0;
+    reservation.id = lastElementIndex + 1;
     this.reservations.push(reservation);
     console.log(this.reservations);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: number): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
     console.log(this.reservations);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
-  editReservation(id: number, modifiedReservation: Reservation): void{
-    let index = this.reservations.findIndex(res => res.id===id);
-    this.reservations[index]=modifiedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations))
+  editReservation(id: number, modifiedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations[index] = modifiedReservation;
   }
 }
